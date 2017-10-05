36bb371eb39de862df2abb3c302e6692cff9470f
Ar11rA <aritra.ghosh2295@yahoo.com>
2017-10-05 12:05:44 +0700

    Updated fixes in webapi playbook

--- test/integration/webapi/webapi.rb
+++ test/integration/webapi/webapi.rb
@@ -2,8 +2,16 @@ describe package('git') do
   it { should be_installed }
 end
 
+describe command('which docker') do
+  its('stdout') { should match /bin\/docker/ }
+end
+
+describe command('docker -v') do
+  its('stdout') { should match /Docker version 17.09/ }
+end
+
 describe command('node -v') do
-  its('stdout') { should match /v6.11.3/ }
+  its('stdout') { should match /v6.11.4/ }
 end
 
 describe command('npm -v') do
@@ -19,4 +27,20 @@ describe npm('cordova') do
   it { should be_installed }
   its('version') { should eq '6.5.0' }
 end
+
+describe file('/usr/sbin/nrsysmond') do
+  it { should exist }
+  it { should be_file }
+end
+
+describe file('/etc/newrelic/nrsysmond.cfg') do
+  it { should exist }
+  it { should be_file }
+end
+
+describe file('/var/run/newrelic/nrsysmond.pid') do
+  it { should exist }
+  it { should be_file }
+end
+
 
